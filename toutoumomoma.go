5d2efdb6e76a9b755750a7dccdc207ef5178d651
Dan Kortschak <dan.kortschak@elastic.co>
2022-11-01 09:43:33 +1030

    address pr comment

--- toutoumomoma.go
+++ toutoumomoma.go
@@ -209,9 +209,11 @@ func (f *File) ImportHash() (hash []byte, imports []string, err error) {
 // The algorithm is analogous to the algorithm described for ImportHash with the exception
 // that Go's static symbols are used in place of the dynamic import symbols used by the
 // ImportHash. The list of symbols referenced by the executable is obtained and the MD5 hash
-// of the ordered list of symbols, separated by commas, is calculated. The fully qualified
-// import path of each symbol is included and while symbols used by ImportHash are
-// canonicalised to lowercase, GoSymbolHash retains the case of the original symbol.
+// of the ordered list of symbols, separated by commas, is calculated. The order of the
+// symbols is as exists in the executable and returned by the standard library debug packages
+// The fully qualified import path of each symbol is included and while symbols used by
+// ImportHash are canonicalised to lowercase, GoSymbolHash retains the case of the original
+// symbol.
 //
 // If the file is an executable, but not a gc-compiled Go executable, ErrNotGoExecutable
 // will be returned.
